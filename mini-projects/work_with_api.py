bd37787aa90606607016b079a2c9a008b7301e97
Spitsin16 <specmaksimus@gmail.com>
2026-05-17 03:35:30 +0300

    Add code to martingale strategy practice

---
+++ mini-projects/work_with_api.py
@@ -0,0 +1,18 @@
+import requests
+import time
+
+url = "https://api.rapira.net/open/market/rates" # ссылка откуда берем данные
+
+bitcoin_prices=[]
+
+for i in range(1):
+    responce = requests.get(url) # получаем данные
+    data = responce.json()  # открываем для чтения
+    rates = data['data']
+    usdt_rub=rates[0]
+    coin=usdt_rub['symbol']
+    price=usdt_rub['close']
+    bitcoin_prices.append(price)
+    time.sleep(1)
+print(bitcoin_prices)
+
